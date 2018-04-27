20ec86e1715e6bb30b38216d50e0ef906e1d94c2
Nick Bradley <nick.bradley.193@pm.me>
2018-04-27 03:44:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -105,10 +105,10 @@ function createRock(x) {
   
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
-  ROCKS.push(rock)
+  ROCKS.push(rock);
 
   // Finally, return the rock element you've created
-  return rock
+  return rock;
 }
 
 /**
@@ -118,6 +118,8 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+  gameInterval = null;
+  
 }
 
 function moveDodger(e) {
